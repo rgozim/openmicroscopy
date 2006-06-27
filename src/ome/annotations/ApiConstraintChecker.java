rgozim/openmicroscopy

commit 24116e124c53ddbe96b9b9c43c24f0a2ffa33f13

Refactored API constraints to ome.api and added generics (#192)
git-svn-id: file:///home/svn/omero/trunk@741 05709c45-44f0-0310-885b-81a1db45b4a6

src/ome/annotations/ApiConstraintChecker.java
@@ -1,184 +1,0 @@
-/*
- * ome.annotations.ApiConstraintChecker
- *
- *------------------------------------------------------------------------------
- *
- *  Copyright (C) 2005 Open Microscopy Environment
- *      Massachusetts Institute of Technology,
- *      National Institutes of Health,
- *      University of Dundee
- *
- *
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *------------------------------------------------------------------------------
- */
-package ome.annotations;
-
-// Java imports
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Collection;
-
-// Third-party libraries
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-// Application-internal dependencies
-import ome.conditions.ApiUsageException;
-import ome.conditions.InternalException;
-import ome.conditions.ValidationException;
-
-/**
- * Checks metadata constraints on API calls.
- * 
- * @author Josh Moore &nbsp;&nbsp;&nbsp;&nbsp; <a
- *         href="mailto:josh.moore@gmx.de">josh.moore@gmx.de</a>
- * @version 1.0 <small> (<b>Internal version:</b> $Rev$ $Date$) </small>
- * @since 1.0
- */
-public class ApiConstraintChecker
-{
-
-    private static Log log = LogFactory.getLog(ApiConstraintChecker.class);
-
-    public static void errorOnViolation(Class implClass, Method mthd, Object[] args) 
-    throws ValidationException
-    {
-
-        /* find concrete method */
-        Method implMethod;
-        try
-        {
-            implMethod = implClass.getMethod(mthd.getName(), (Class[]) mthd
-                    .getParameterTypes());
-        } catch (SecurityException e)
-        {
-            throw new InternalException(
-                    "Not allowed to perform reflection for testing API.\n" +
-                    String.format("Class:%s Method:%s",implClass.getName(),mthd));
-        } catch (NoSuchMethodException e)
-        {
-            return; // TODO No method == no violation. 
-        }
-
-        log.info("Checking: " + implMethod);
-
-        /* get arrays of arguments with parameters */
-        if ( args == null )
-            args = new Object[]{};
-        
-        Annotation[][] anns = implMethod.getParameterAnnotations();
-
-        for (int i = 0; i < args.length; i++)
-        {
-            Object arg = args[i];
-            Annotation[] annotations = anns[i];
-
-            boolean validated = false;
-
-            for (Annotation annotation : annotations)
-            {
-                if (NotNull.class.equals(annotation.annotationType()))
-                {
-                    if (null == arg)
-                    {
-                        String msg = "Argument " + i + " to " + implMethod
-                                + " may not be null.";
-                        log.warn(msg);
-                        throw new ApiUsageException(msg);
-
-                    }
-                }
-
-                else if (Validate.class.equals(annotation.annotationType()))
-                {
-                    validated = true;
-                    Validate validator = (Validate) annotation;
-                    Class[] validClasses = validator.value();
-                    ValidSet validSet = new ValidSet( validClasses );
-
-                    String msg = "Argument " + i + " must be of a type in:"
-                            + validSet;
-
-                    if (null == arg) {
-                        // handled by NotNull
-                    }
-                    
-                    else if (arg instanceof Collection)
-                    {
-                        Collection coll = (Collection) arg;
-                        for (Object object : coll)
-                        {
-                            if ( ! validSet.isValid( object.getClass() ))
-                                throw new ApiUsageException(msg);
-                        }
-
-                    }
-
-                    else
-                    {
-                        if ( ! validSet.isValid( arg.getClass() ))
-                        {
-                            throw new ApiUsageException(msg);
-                        }
-                    }
-                }
-            }
-
-            /* warn if someone's forgotten to annotate a method */
-            if (arg instanceof Collection && !validated)
-                throw new ValidationException(implMethod
-                        + " is missing a required @" + Validate.class.getName()
-                        + " annotation. Refusing to proceed...");
-
-        }
-
-    }
-}
-
-class ValidSet {
- 
-    Class[] classes;
-    
-    public ValidSet( Class[] validClasses )
-    {
-        classes = validClasses;
-    }
-    
-    public boolean isValid( Class target )
-    {
-        if ( classes == null )
-            return false;
-        
-        for (int i = 0; i < classes.length; i++)
-        {
-            Class klass = classes[i];
-            if ( klass == null)
-                continue;
-            
-            if ( klass.isAssignableFrom( target ))
-                return true;
-        }
-        return false;
-    }
-    
-    public String toString()
-    {
-        return Arrays.asList(classes).toString();
-    }
-}

src/ome/annotations/NotNull.java
@@ -1,57 +1,0 @@
-/*
- * ome.annotations.Validate
- *
- *------------------------------------------------------------------------------
- *
- *  Copyright (C) 2005 Open Microscopy Environment
- *      Massachusetts Institute of Technology,
- *      National Institutes of Health,
- *      University of Dundee
- *
- *
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *------------------------------------------------------------------------------
- */
-package ome.annotations;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-//Java imports
-
-//Third-party libraries
-
-//Application-internal dependencies
-
-
-/** 
- * annotation used for determining the types of a Set 
- * @author  Josh Moore &nbsp;&nbsp;&nbsp;&nbsp;
- * 				<a href="mailto:josh.moore@gmx.de">josh.moore@gmx.de</a>
- * @version 1.0 
- * <small>
- * (<b>Internal version:</b> $Rev$ $Date$)
- * </small>
- * @since 1.0
- */
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.PARAMETER)
-public @interface NotNull{
-}
-

src/ome/annotations/Validate.java
@@ -1,58 +1,0 @@
-/*
- * ome.annotations.Validate
- *
- *------------------------------------------------------------------------------
- *
- *  Copyright (C) 2005 Open Microscopy Environment
- *      Massachusetts Institute of Technology,
- *      National Institutes of Health,
- *      University of Dundee
- *
- *
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *------------------------------------------------------------------------------
- */
-package ome.annotations;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-//Java imports
-
-//Third-party libraries
-
-//Application-internal dependencies
-
-
-/** 
- * annotation used for determining the types of a Set 
- * @author  Josh Moore &nbsp;&nbsp;&nbsp;&nbsp;
- * 				<a href="mailto:josh.moore@gmx.de">josh.moore@gmx.de</a>
- * @version 1.0 
- * <small>
- * (<b>Internal version:</b> $Rev$ $Date$)
- * </small>
- * @since 1.0
- */
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.PARAMETER)
-public @interface Validate{
-    Class[] value();
-}
-

src/ome/logic/PixelsImpl.java
@@ -42,6 +42,7 @@ import org.springframework.transaction.annotation.Transactional;
 import ome.api.IPixels;
 import ome.io.nio.PixelBuffer;
 import ome.io.nio.PixelsService;
+import ome.model.IObject;
 import ome.model.core.Pixels;
 import ome.model.display.RenderingDef;
 import ome.model.enums.PixelsType;
@@ -107,12 +108,12 @@ class PixelsImpl extends AbstractLevel2Service
         return PixelBuffer.getBitDepth( pixelsType );
     }
 
-    public Object getEnumeration(Class klass, String value)
+    public <T extends IObject> T getEnumeration(Class<T> klass, String value)
     {
     	return iQuery.findByString(klass, "value", value);
     }
 
-    public List getAllEnumerations(Class klass)
+    public <T extends IObject> List<T> getAllEnumerations(Class<T> klass)
     {
     	return iQuery.findAll(klass, null);
     }

src/ome/logic/PojosImpl.java
@@ -51,8 +51,6 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.transaction.annotation.Transactional;
 
 //Application-internal dependencies
-import ome.annotations.NotNull;
-import ome.annotations.Validate;
 import ome.api.IPojos;
 import ome.conditions.ApiUsageException;
 import ome.conditions.InternalException;
@@ -106,7 +104,7 @@ public class PojosImpl extends AbstractLevel2Service implements IPojos
     
     @Transactional(readOnly = true)
     public Set loadContainerHierarchy(Class rootNodeType, 
-            @Validate(Long.class) Set rootNodeIds, Map options) {
+            Set rootNodeIds, Map options) {
         
         PojoOptions po = new PojoOptions(options);
         
@@ -141,8 +139,8 @@ public class PojosImpl extends AbstractLevel2Service implements IPojos
     
     @Transactional(readOnly=true)
 	public Set findContainerHierarchies(
-            @NotNull final Class rootNodeType, 
-            @NotNull @Validate(Long.class) final Set imageIds, 
+            final Class rootNodeType, 
+            final Set imageIds, 
             Map options) {
 		
 		PojoOptions po = new PojoOptions(options);
@@ -204,9 +202,9 @@ public class PojosImpl extends AbstractLevel2Service implements IPojos
 
     @Transactional(readOnly=true)
 	public Map findAnnotations(
-            @NotNull Class rootNodeType, 
-            @NotNull @Validate(Long.class) Set rootNodeIds, 
-            @Validate(Long.class) Set annotatorIds, Map options) {
+            Class rootNodeType, 
+            Set rootNodeIds, 
+            Set annotatorIds, Map options) {
 		
         if (rootNodeIds.size()==0)
             return new HashMap();
@@ -252,7 +250,7 @@ public class PojosImpl extends AbstractLevel2Service implements IPojos
 	}
 
     @Transactional(readOnly=true)
-	public Set findCGCPaths(@NotNull @Validate(Long.class) Set imgIds, 
+	public Set findCGCPaths(Set imgIds, 
             String algorithm, Map options) {
 
 		if (imgIds.size()==0){
@@ -317,8 +315,8 @@ public class PojosImpl extends AbstractLevel2Service implements IPojos
 	}
 
     @Transactional(readOnly=true)
-	public Set getImages(@NotNull Class rootNodeType, 
-            @NotNull @Validate(Long.class) Set rootNodeIds, Map options) {
+	public Set getImages(Class rootNodeType, 
+            Set rootNodeIds, Map options) {
 		
 		if (rootNodeIds.size()==0){
 			return new HashSet();
@@ -363,7 +361,7 @@ public class PojosImpl extends AbstractLevel2Service implements IPojos
 	}
     
     @Transactional(readOnly=true)
-    public Map getUserDetails(@NotNull @Validate(String.class) Set names, 
+    public Map getUserDetails(Set names, 
             Map options)
     {
         
@@ -403,8 +401,8 @@ public class PojosImpl extends AbstractLevel2Service implements IPojos
     }
     
     @Transactional(readOnly=true)
-    public Map getCollectionCount(@NotNull String type, @NotNull String property, 
-            @NotNull @Validate(Long.class) Set ids, Map options)
+    public Map getCollectionCount(String type, String property, 
+            Set ids, Map options)
     {
         
         String parsedProperty = LsidUtils.parseField(property);

src/ome/logic/QueryImpl.java
@@ -52,7 +52,6 @@ import org.hibernate.criterion.MatchMode;
 import org.hibernate.metadata.ClassMetadata;
 
 //Application-internal dependencies
-import ome.annotations.NotNull;
 import ome.api.IQuery;
 import ome.api.local.LocalQuery;
 import ome.conditions.ApiUsageException;
@@ -138,7 +137,7 @@ public class QueryImpl extends AbstractLevel1Service implements LocalQuery {
      * @DEV.TODO weirdness here; learn more about CGLIB initialization.
      */
     @SuppressWarnings("unchecked")
-    public IObject get(@NotNull final Class klass, final long id) 
+    public IObject get(final Class klass, final long id) 
     throws ValidationException
     {
         return (IObject) getHibernateTemplate().execute(new HibernateCallback() {
@@ -171,7 +170,7 @@ public class QueryImpl extends AbstractLevel1Service implements LocalQuery {
      * @DEV.TODO weirdness here; learn more about CGLIB initialization.
      */
     @SuppressWarnings("unchecked")
-    public IObject find(@NotNull final Class klass, final long id)
+    public IObject find(final Class klass, final long id)
     {
         return (IObject) getHibernateTemplate().execute(new HibernateCallback() {
             public Object doInHibernate(Session session)
@@ -189,7 +188,7 @@ public class QueryImpl extends AbstractLevel1Service implements LocalQuery {
     /**
      * @see ome.api.IQuery#getByClass(java.lang.Class)
      */
-    public List findAll(@NotNull final Class klass, final Filter filter)
+    public List findAll(final Class klass, final Filter filter)
     {
         if ( filter == null )
             return getHibernateTemplate().loadAll( klass );
@@ -208,7 +207,7 @@ public class QueryImpl extends AbstractLevel1Service implements LocalQuery {
     /**
      * @see ome.api.IQuery#findByExample(ome.model.IObject)
      */
-    public IObject findByExample(@NotNull final IObject example) throws ApiUsageException
+    public IObject findByExample(final IObject example) throws ApiUsageException
     {
         return (IObject) getHibernateTemplate().execute(new HibernateCallback() {
             public Object doInHibernate(Session session)
@@ -225,7 +224,7 @@ public class QueryImpl extends AbstractLevel1Service implements LocalQuery {
     /**
      * @see ome.api.IQuery#findAllByExample(ome.model.IObject, ome.parameters.Filter)
      */
-    public List findAllByExample(@NotNull final IObject example, final Filter filter)
+    public List findAllByExample(final IObject example, final Filter filter)
     {
         return (List) getHibernateTemplate().execute(new HibernateCallback() {
             public Object doInHibernate(Session session)
@@ -244,8 +243,8 @@ public class QueryImpl extends AbstractLevel1Service implements LocalQuery {
      * @see ome.api.IQuery#findByString(java.lang.Class, java.lang.String, java.lang.String)
      */
     public IObject findByString(
-    @NotNull final Class klass, 
-    @NotNull final String fieldName, 
+    final Class klass, 
+    final String fieldName, 
     final String value) 
     throws ApiUsageException
     {
@@ -265,8 +264,8 @@ public class QueryImpl extends AbstractLevel1Service implements LocalQuery {
      * @see ome.api.IQuery#findAllByString(java.lang.Class, java.lang.String, java.lang.String, boolean, ome.parameters.Filter)
      */
     public List findAllByString(
-            @NotNull final Class klass, 
-            @NotNull final String fieldName, 
+            final Class klass, 
+            final String fieldName, 
             final String value, 
             final boolean caseSensitive, 
             final Filter filter) 
@@ -293,7 +292,7 @@ public class QueryImpl extends AbstractLevel1Service implements LocalQuery {
     /** 
      * @see ome.api.IQuery#findByQuery(java.lang.String, ome.parameters.Parameters)
      */
-    public IObject findByQuery(@NotNull String queryName, Parameters params) 
+    public IObject findByQuery(String queryName, Parameters params) 
     throws ValidationException
     {
         
@@ -331,7 +330,7 @@ public class QueryImpl extends AbstractLevel1Service implements LocalQuery {
     /** 
      * @see ome.api.IQuery#findAllByQuery(java.lang.String, ome.parameters.Parameters)
      */
-    public List findAllByQuery(@NotNull String queryName, Parameters params)
+    public List findAllByQuery(String queryName, Parameters params)
     {
         Query<List> q = queryFactory.lookup( queryName, params );
         return execute(q);

src/ome/logic/UpdateImpl.java
@@ -55,7 +55,6 @@ import org.springframework.orm.hibernate3.SessionFactoryUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 // Application-internal dependencies
-import ome.annotations.Validate;
 import ome.api.IUpdate;
 import ome.api.local.LocalUpdate;
 import ome.model.IObject;
@@ -141,7 +140,7 @@ public class UpdateImpl extends AbstractLevel1Service implements LocalUpdate
         return graph;
     }
 
-    public void saveCollection(@Validate(IObject.class) Collection graph)
+    public void saveCollection(Collection graph)
     {
         UpdateFilter filter = new UpdateFilter( getHibernateTemplate() );
         beforeUpdate( graph, filter );
@@ -153,8 +152,7 @@ public class UpdateImpl extends AbstractLevel1Service implements LocalUpdate
         afterUpdate( graph, filter );
     }
     
-    public Collection saveAndReturnCollection(
-            @Validate(IObject.class) Collection graph)
+    public Collection saveAndReturnCollection( Collection graph)
     {
         throw new RuntimeException("Not implemented yet.");
     }

test/ome/server/utests/ApiConstraintCheckerTest.java
@@ -1,0 +1,162 @@
+/*
+ * ome.server.utests.ApiConstraintCheckerTest
+ *
+ *------------------------------------------------------------------------------
+ *
+ *  Copyright (C) 2005 Open Microscopy Environment
+ *      Massachusetts Institute of Technology,
+ *      National Institutes of Health,
+ *      University of Dundee
+ *
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *------------------------------------------------------------------------------
+ */
+package ome.server.utests;
+
+//Java imports
+
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import org.testng.annotations.*;
+
+import ome.annotations.ApiConstraintChecker;
+import ome.annotations.NotNull;
+import ome.annotations.Validate;
+import ome.conditions.ApiUsageException;
+import ome.conditions.ValidationException;
+import ome.logic.PojosImpl;
+import ome.model.IObject;
+import ome.model.containers.Project;
+
+
+//Third-party libraries
+import junit.framework.TestCase;
+
+//Application-internal dependencies
+
+
+/**
+ * @author  Josh Moore &nbsp;&nbsp;&nbsp;&nbsp;
+ * 				<a href="mailto:josh.moore@gmx.de">josh.moore@gmx.de</a>
+ * @version 1.0 
+ * <small>
+ * (<b>Internal version:</b> $Rev$ $Date$)
+ * </small>
+ * @since Omero 3.0
+ */
+public class ApiConstraintCheckerTest extends TestCase {
+
+    Class c;
+    Method m;
+    Object[] args;
+    
+    @Test
+    @ExpectedExceptions(ApiUsageException.class)
+    public void testNullCheck() throws Exception {
+        ApiConstraintChecker.errorOnViolation(null,null,null);
+    }
+ 
+    /*   
+         public Set<IObject> loadContainerHierarchy(
+         @NotNull Class<IObject> rootNodeType, 
+         @Validate(Long.class) Set<Long> rootNodeIds,
+         Map options);
+    */
+
+    @Test
+    @ExpectedExceptions(ApiUsageException.class)
+    public void testNullClass() throws Exception {
+        loadContainerHierarchy();
+        args = new Object[]{null, Collections.EMPTY_SET, null};
+        ApiConstraintChecker.errorOnViolation(c,m,args);
+    }
+    
+    @Test
+    public void testNullSet() throws Exception {
+        loadContainerHierarchy();
+        args = new Object[]{Project.class, null, null};
+        ApiConstraintChecker.errorOnViolation(c,m,args);
+    }
+    
+    @Test
+    @ExpectedExceptions(ApiUsageException.class)
+    public void testIntegersInSet() throws Exception {
+        loadContainerHierarchy();
+        args = new Object[]{Project.class, Collections.singleton( new Integer(1) ), null};
+        ApiConstraintChecker.errorOnViolation(c,m,args);
+    }
+
+    /*
+        public <T extends IObject> Set<IObject> findContainerHierarchies(
+            @NotNull Class<T> rootNodeType, 
+            @NotNull @Validate(Long.class) Set<Long> imagesIds,
+            Map options);
+     */
+
+    @Test
+    @ExpectedExceptions(ApiUsageException.class)
+    public void testNullClass_find() throws Exception {
+        findContainerHierarchies();
+        args = new Object[]{null, Collections.EMPTY_SET, null};
+        ApiConstraintChecker.errorOnViolation(c,m,args);
+    }
+    
+    @Test
+    @ExpectedExceptions(ApiUsageException.class)
+    public void testNullSet_find() throws Exception {
+        findContainerHierarchies();
+        args = new Object[]{Project.class, null, null};
+        ApiConstraintChecker.errorOnViolation(c,m,args);
+    }
+    
+    @Test
+    @ExpectedExceptions(ApiUsageException.class)
+    public void testIntegersInSet_find() throws Exception {
+        findContainerHierarchies();
+        args = new Object[]{Project.class, Collections.singleton( new Integer(1) ), null};
+        ApiConstraintChecker.errorOnViolation(c,m,args);
+    }
+    
+    @Test
+    public void testGood_find() throws Exception {
+        findContainerHierarchies();
+        args = new Object[]{Project.class, Collections.singleton( new Long(1) ), null};
+        ApiConstraintChecker.errorOnViolation(c,m,args);
+    }
+    
+    // ~ Helpers
+    // =========================================================================
+
+    
+    private void loadContainerHierarchy() throws NoSuchMethodException, SecurityException
+    {
+        c = PojosImpl.class;
+        m = c.getMethod("loadContainerHierarchy",Class.class,Set.class,Map.class);
+    }
+
+    private void findContainerHierarchies() throws NoSuchMethodException, SecurityException
+    {
+        c = PojosImpl.class;
+        m = c.getMethod("findContainerHierarchies",Class.class,Set.class,Map.class);
+    }
+
+    
+}

commit 33280e497a5b5f055359e29e94ea76e6472146e5

NPE caused by #192. Hopefully fixed.
git-svn-id: file:///home/svn/omero/trunk@743 05709c45-44f0-0310-885b-81a1db45b4a6

src/ome/annotations/ApiConstraintChecker.java
@@ -80,7 +80,8 @@ public class ApiConstraintChecker
         for (int j = 0; j < allAnnotations.length; j++)
         {
             Annotation[][] anns = (Annotation[][]) allAnnotations[j];            
-
+            if (anns == null) continue;
+            
             for (int i = 0; i < args.length; i++)
             {
                 Object arg = args[i];