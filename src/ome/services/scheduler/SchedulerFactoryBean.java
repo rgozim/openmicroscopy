rgozim/openmicroscopy

commit 772d7fc8e1700cc0bcb40d58bbabc64c95ebf349

Workaround for multiple `ContextRefreshedEvents` when using parent contexts
git-svn-id: file:///home/svn/omero/trunk@8715 05709c45-44f0-0310-885b-81a1db45b4a6

src/ome/services/scheduler/SchedulerFactoryBean.java
@@ -10,6 +10,8 @@ package ome.services.scheduler;
 import java.util.HashMap;
 import java.util.Map;
 
+import ome.tools.spring.OnContextRefreshedEventListener;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.quartz.JobDetail;
@@ -31,29 +33,43 @@ import org.springframework.scheduling.quartz.JobDetailAwareTrigger;
  */
 public class SchedulerFactoryBean extends
         org.springframework.scheduling.quartz.SchedulerFactoryBean implements
-        ApplicationListener {
+        ApplicationListener<ContextRefreshedEvent> {
 
     private final static Log log = LogFactory
             .getLog(SchedulerFactoryBean.class);
 
     private final Map<String, Trigger> triggers = new HashMap<String, Trigger>();
 
-    public void onApplicationEvent(ApplicationEvent event) {
-        if (event instanceof ContextRefreshedEvent) {
-            ContextRefreshedEvent cre = (ContextRefreshedEvent) event;
-            String[] names = cre.getApplicationContext().getBeanNamesForType(
-                    Trigger.class);
-            for (String name : names) {
-                if (triggers.containsKey(name)) {
-                    log.error("Scheduler already has trigger named: " + name);
-                    continue;
-                }
-                Trigger trigger = (Trigger) cre.getApplicationContext()
-                        .getBean(name);
-                registerTrigger(name, trigger);
+    /**
+     * Already subclassing another class, so re-using the handler here
+     * in a somewhat awkward to re-use the code.
+     */
+    private final OnContextRefreshedEventListener handler =
+        new OnContextRefreshedEventListener() {
+
+            @Override
+            public void handleContextRefreshedEvent(ContextRefreshedEvent event) {
+                handle(event);
             }
-            restartIfNeeded();
+        };
+
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        handler.onApplicationEvent(event);
+    }
+
+    private void handle(ContextRefreshedEvent cre) {
+        String[] names = cre.getApplicationContext().getBeanNamesForType(
+                Trigger.class);
+        for (String name : names) {
+            if (triggers.containsKey(name)) {
+                log.error("Scheduler already has trigger named: " + name);
+                continue;
+            }
+            Trigger trigger = (Trigger) cre.getApplicationContext()
+                    .getBean(name);
+            registerTrigger(name, trigger);
         }
+        restartIfNeeded();
     }
 
     /**

src/ome/services/util/NamespaceCheck.java
@@ -16,6 +16,7 @@ import ome.parameters.Parameters;
 import ome.system.Principal;
 import ome.system.Roles;
 import ome.system.ServiceFactory;
+import ome.tools.spring.OnContextRefreshedEventListener;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -31,8 +32,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @author Josh Moore, josh at glencoesoftwarecom
  * @since 4.2.1
  */
-public class NamespaceCheck implements
-        ApplicationListener<ContextRefreshedEvent> {
+public class NamespaceCheck extends OnContextRefreshedEventListener {
 
     public final static Log log = LogFactory.getLog(NamespaceCheck.class);
 
@@ -50,7 +50,8 @@ public class NamespaceCheck implements
         this.roles = roles;
     }
 
-    public void onApplicationEvent(ContextRefreshedEvent arg0) {
+    @Override
+    public void handleContextRefreshedEvent(ContextRefreshedEvent event) {
         executor.execute(principal, new Executor.SimpleWork(this,
                 "namespaceCheck") {
             @Transactional(readOnly = false)

src/ome/tools/hibernate/ExtendedMetadata.java
@@ -22,6 +22,7 @@ import ome.model.IAnnotated;
 import ome.model.IObject;
 import ome.model.annotations.Annotation;
 import ome.model.internal.Permissions;
+import ome.tools.spring.OnContextRefreshedEventListener;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -121,7 +122,7 @@ public interface ExtendedMetadata {
  * Sole implementatino of ExtendedMetadata. The separation is intended to make
  * unit testing without a full {@link ExtendedMetadata} possible.
  */
-public static class Impl implements ExtendedMetadata, ApplicationListener {
+public static class Impl extends OnContextRefreshedEventListener implements ExtendedMetadata {
 
     private final static Log log = LogFactory.getLog(ExtendedMetadata.class);
 
@@ -159,14 +160,8 @@ public static class Impl implements ExtendedMetadata, ApplicationListener {
      * extracts the {@link SessionFactory} from the {@link ApplicationContext}
      * and pases it to {@link #setSessionFactory(SessionFactory)}.
      */
-    public void onApplicationEvent(
-            org.springframework.context.ApplicationEvent event) {
-
-        if (!(event instanceof ContextRefreshedEvent)) {
-            return; // EARLY EXIT!!
-        }
-
-        ContextRefreshedEvent cre = (ContextRefreshedEvent) event;
+    @Override
+    public void handleContextRefreshedEvent(ContextRefreshedEvent cre) {
         ApplicationContext ctx = cre.getApplicationContext();
         if (ctx.containsBean("sessionFactory")) {
             SessionFactory sessionFactory = (SessionFactory) ctx

src/ome/tools/spring/OnContextRefreshedEventListener.java
@@ -1,0 +1,72 @@
+/*   $Id$
+ *
+ *   Copyright 2007 Glencoe Software, Inc. All rights reserved.
+ *   Use is subject to license terms supplied in LICENSE.txt
+ */
+package ome.tools.spring;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+/**
+ * {@link ApplicationListener} which only listens for
+ * {@link ContextRefreshedEvent} <em>and</em> only responds to the first one.
+ *
+ * @author Josh Moore, josh at glencoesoftware.com
+ * @since 4.3.0
+ */
+public abstract class OnContextRefreshedEventListener implements
+        ApplicationListener<ContextRefreshedEvent>, ApplicationContextAware {
+
+    private final Log log = LogFactory.getLog(getClass());
+
+    private/* final */ApplicationContext ctx;
+
+    private final boolean handleOthers;
+
+    private final int limit;
+
+    private final AtomicInteger count = new AtomicInteger(0);
+
+    public OnContextRefreshedEventListener() {
+        this(false, 1);
+    }
+
+    public OnContextRefreshedEventListener(boolean handleOthers, int limit) {
+        this.handleOthers = handleOthers;
+        this.limit = limit;
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext)
+            throws BeansException {
+        this.ctx = applicationContext;
+    }
+
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (event.getApplicationContext() != ctx && !handleOthers) {
+            if (log.isDebugEnabled()) {
+                log.debug("Ignoring other application context refresh: " + ctx);
+            }
+            return;
+        }
+
+        int current = count.incrementAndGet();
+        if (current > limit) {
+            if (log.isDebugEnabled()) {
+                log.debug("Ignoring refresh beyond limit: " + current);
+            }
+            return;
+        }
+        handleContextRefreshedEvent(event);
+    }
+
+    public abstract void handleContextRefreshedEvent(ContextRefreshedEvent event);
+
+}

commit 013b0a4fd53ca189119402495e54786e3a0b2644

Scheduler broken since r8715 due to missed `ContextRefreshedEvent` (Fix #3717, #3719)
Since the internal handler had a null context, the check for
`event.getContext() != ctx` always returned true.

git-svn-id: file:///home/svn/omero/trunk@8762 05709c45-44f0-0310-885b-81a1db45b4a6

src/ome/services/scheduler/SchedulerFactoryBean.java
@@ -18,6 +18,8 @@ import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -33,7 +35,8 @@ import org.springframework.scheduling.quartz.JobDetailAwareTrigger;
  */
 public class SchedulerFactoryBean extends
         org.springframework.scheduling.quartz.SchedulerFactoryBean implements
-        ApplicationListener<ContextRefreshedEvent> {
+        ApplicationListener<ContextRefreshedEvent>,
+        ApplicationContextAware {
 
     private final static Log log = LogFactory
             .getLog(SchedulerFactoryBean.class);
@@ -52,6 +55,10 @@ public class SchedulerFactoryBean extends
                 handle(event);
             }
         };
+
+    public void setApplicationContext(ApplicationContext ctx) {
+        handler.setApplicationContext(ctx);
+    }
 
     public void onApplicationEvent(ContextRefreshedEvent event) {
         handler.onApplicationEvent(event);